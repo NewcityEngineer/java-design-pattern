b27e453645367e5ea28b1aa5d9fe2ea8ca7d91e1
tianlixin <newcitysoft@163.com>
2018-02-23 10:57:33 +0800

    增加一个桥接模式中画图API的实现类。

--- src/com/newcitysoft/study/design_pattern/structural/bridge/BridgePatternDemo.java
+++ src/com/newcitysoft/study/design_pattern/structural/bridge/BridgePatternDemo.java
@@ -1,6 +1,7 @@
 package com.newcitysoft.study.design_pattern.structural.bridge;
 
 import com.newcitysoft.study.design_pattern.structural.bridge.circle.GreenCircle;
+import com.newcitysoft.study.design_pattern.structural.bridge.circle.GreyCircle;
 import com.newcitysoft.study.design_pattern.structural.bridge.circle.RedCircle;
 import com.newcitysoft.study.design_pattern.structural.bridge.shape.Circle;
 import com.newcitysoft.study.design_pattern.structural.bridge.shape.Shape;
@@ -15,8 +16,10 @@ public class BridgePatternDemo {
     public static void main(String[] args){
         Shape redCircle = new Circle(1, 2, 3, new RedCircle());
         Shape greenCircle = new Circle(2, 3, 4, new GreenCircle());
+        Shape greyCircle = new Circle(3, 4, 5, new GreyCircle());
 
         redCircle.draw();
         greenCircle.draw();
+        greyCircle.draw();
     }
 }
