e4183e373da020630cd4de99e5d5b9a3caedd3e2
tianlixin <newcitysoft@163.com>
2018-02-23 10:49:18 +0800

    增加桥接模式的代码实现

---
+++ src/com/newcitysoft/study/design_pattern/structural/bridge/BridgePatternDemo.java
@@ -0,0 +1,22 @@
+package com.newcitysoft.study.design_pattern.structural.bridge;
+
+import com.newcitysoft.study.design_pattern.structural.bridge.circle.GreenCircle;
+import com.newcitysoft.study.design_pattern.structural.bridge.circle.RedCircle;
+import com.newcitysoft.study.design_pattern.structural.bridge.shape.Circle;
+import com.newcitysoft.study.design_pattern.structural.bridge.shape.Shape;
+
+/**
+ * 桥接模式Demo类
+ * @author lixin.tian@renren-inc.com
+ * @date 2018/2/23 10:43
+ */
+public class BridgePatternDemo {
+
+    public static void main(String[] args){
+        Shape redCircle = new Circle(1, 2, 3, new RedCircle());
+        Shape greenCircle = new Circle(2, 3, 4, new GreenCircle());
+
+        redCircle.draw();
+        greenCircle.draw();
+    }
+}
